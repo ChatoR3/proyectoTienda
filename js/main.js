9c836e2d92a99b2193945b4aceff6cdcfea0807e
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-01-25 17:15:33 -0600

    Se fusiona el mobilemenu.html con el index.html, se hacen cambios a los archivos indexDesktop.css e indexMobile.css, se crean eventos para desplegar los menús mobile y desktop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    console.log('click');
+    mobileMenu.classList.toggle('inactive');
+}
+
 console.log('JavaScript Funcionando');
