de4adc42dfcf081969e34037c9b310b8ae2e05a6
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-01-30 11:59:01 -0600

    Se empieza a actualizar la parte de Shopping Cart, la parte de aside

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 burgerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toogleCarritoAside);
+
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
