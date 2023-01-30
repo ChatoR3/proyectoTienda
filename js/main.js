cd2798f4072f6c6eb77d74ad28fe221e8c7bc859
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-01-30 16:54:53 -0600

    Se termina de actializar index.html, agregando el elemento aside y se termina de agregar los eventos para abrir y cerrar el carrito de compras, menú desktop y menú mobile

--- js/main.js
+++ js/main.js
@@ -11,12 +11,41 @@ shoppingCartIcon.addEventListener('click', toogleCarritoAside);
 
 
 function toogleDesktopMenu(){
+    console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
     console.log('click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarritoAside(){
+    console.log('click');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
 
 console.log('JavaScript Funcionando');
