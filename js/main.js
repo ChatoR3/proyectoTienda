336f7fd59908a2b88ba4b83aa65d756043c86c5a
ChatoR3 <ing.rodriguez.rivera@gmail.com>
2023-02-08 14:04:25 -0600

    Se agregaron comentarios al archivo main.js

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ function toogleCarritoAside(){
 }
 
 
+// Creamos Lista de productos manualmente
 const productList = [];
 
 productList.push({
@@ -60,14 +61,14 @@ productList.push({
 
 productList.push({
     name: 'Bicicleta',
-    price: 220,
+    price: 180,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
 productList.push({
     name: 'Computadora',
-    price: 180,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    price: 1500,
+    image: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 });
 
 /*
@@ -86,23 +87,27 @@ productList.push({
 
 */
 
+// Atajo para crear ciclo FOR =     for (product of productList), el elemento product será cada objeto que esté dentro de nuestro array
+                                    // prduct of productList, nos muestra los nombres de los productos
+// Otra forma es for (product in productList), nos muestra el índice en lugar del nombre
+
 function renderProducts(arr){
     for (product of arr) {
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
+        const productCard = document.createElement('div');  //document.createElement nos crea cada elemento html con javascript ()
+        productCard.classList.add('product-card');  // Se agrega la clase que tiene nuestro div en html
      
+    //Creamos nuestro código html a través de JavaScript
      // product = {name, price, image} -> product.image
         const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.image);
+        productImg.setAttribute('src', product.image);  //Se modifica el atributo src con la información de nuestro array
         
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
      
-     
         const productInfoDiv = document.createElement('div');
      
         const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
+        productPrice.innerText = '$' + product.price;   //Propiedad .innerText agrega texto
         const productName = document.createElement('p');
         productName.innerText = product.name;
      
@@ -111,17 +116,19 @@ function renderProducts(arr){
      
         const productInfoFigure = document.createElement('figure');
         const productImgCart = document.createElement('img');
-        productImgCart.setAttribute('src', './img/icons/bt_add_to_cart.svg');
+        productImgCart.setAttribute('src', './img/icons/bt_add_to_cart.svg'); //Este atributo no está en el array por lo que se 
+                                                                              //coloca la ubicación donde se encuentra el elemento
+        
+
+        productInfoFigure.appendChild(productImgCart); // contenedor img se agrega dentro del elemento (tag/etiqueta) figure
      
-        productInfoFigure.appendChild(productImgCart);
+        productInfo.appendChild(productInfoDiv); // contenedor productInfoDiv se agrega al elemento (tag/etiqueta) productInfo
+        productInfo.appendChild(productInfoFigure); //contenedor productInfoFigure se agrega al elemento (tag/etiqueta) productInfo
      
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-     
-        productCard.appendChild(productImg);
+        productCard.appendChild(productImg);  //contenedor productImg y productInfo se agregan al elemento (tag/etiqueta) produtCard
         productCard.appendChild(productInfo);
      
-        cardsContainer.appendChild(productCard);
+        cardsContainer.appendChild(productCard); //contenedor productCard se agrega al elemento (tag/etiqueta) cardsContainer
      }
      
 }
